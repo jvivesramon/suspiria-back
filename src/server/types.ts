40c7aaa314298fa216d463da45d67bd86affe4c3
Judit Vives <jvivesramon@gmail.com>
2023-05-25 23:18:08 +0200

    Add loginController test

--- src/server/types.ts
+++ src/server/types.ts
@@ -3,6 +3,11 @@ import { type Request } from "express";
 export interface UserCredentials {
   username: string;
   password: string;
+  name: string;
+}
+
+export interface UserUniqueCredentials extends UserCredentials {
+  _id: string;
 }
 
 export type UserCredentialsRequest = Request<
