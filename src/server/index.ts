4ff4ec60313da022573a2e570609a2fb542009af
Judit Vives <jvivesramon@gmail.com>
2023-05-25 12:45:33 +0200

    Add allowed origins at cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,16 @@
+import "..loadEnviroment.js";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(corsOptions));
 
 app.disable("x-powered-by");
 
