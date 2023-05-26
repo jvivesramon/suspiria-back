3ab768d686a0855bbe96d6842ca58995e2a0350a
Judit Vives <jvivesramon@gmail.com>
2023-05-26 12:15:56 +0200

    Add validation at loginController controller

--- src/server/routers/userRouter.ts
+++ src/server/routers/userRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { validate } from "express-validation";
 import path from "../utils/paths/paths.js";
 import loginController from "../controllers/userControllers.ts/userControllers.js";
+import { loginSchema } from "../schemas/UserSchema.js";
 
 const userRouter = Router();
 
-userRouter.post(path.login, loginController);
+userRouter.post(
+  path.login,
+  validate(loginSchema, {}, { abortEarly: false }),
+  loginController
+);
 
 export default userRouter;
